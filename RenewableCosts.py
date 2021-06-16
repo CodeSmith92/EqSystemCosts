7f31e28c2a1b1171a4b1cffa94a9a7ed04f9ef84
DC Smith <45472555+CodeSmith92@users.noreply.github.com>
2021-06-16 02:16:24 -0400

    Add files via upload
    preliminary

---
+++ RenewableCosts.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import os
+
+# TODO: finish writing the following functions
+
+def getWindCAPEX(x,y):
+    """ calculate land-based wind capital expenditure (CAPEX) based on geographic location """
+
+def getWindCosts ():
+    """ calculate the total cost (build + annual operation) of wind farms """
+
+def getSolarCosts ():
+    """ calculate the total cost (build + annual operation) of utility solar PV farms   """
