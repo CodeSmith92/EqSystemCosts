255e4563a0a0a2b2f447354d2e7a7d4a38b196a6
DC Smith <45472555+CodeSmith92@users.noreply.github.com>
2021-07-06 00:02:46 -0400

    Update Wind.py

--- Wind.py
+++ Wind.py
@@ -5,39 +5,39 @@ import geopandas as gpd
 from shapely.geometry import Point
 import os
 
-parser = argparse.ArgumentParser(description='TBD')
+
+local_path = os.path.dirname(os.path.abspath(__file__))
+
+# CLI arguments
+parser = argparse.ArgumentParser(description='Command line arguments for data extraction and cost calculations')
 parser.add_argument('--year', type=int, choices=[2010, 2011, 2012, 2013, 2014], help='Data year. Must be in '
                                                                                      '2010-2014 (inclusive).')
-parser.add_argument('--api_key', type=str, help='NREL API Key. Sign up @ '
-                                                'https://developer.nrel.gov/signup/',
-                    default='qgjZ5sr5MHoI26rC69QfLhIM9T5xsRGHwPiGWi8j')
-parser.add_argument('--email', type=str, help='Email address.', default='dcorrell@umich.edu')
-parser.add_argument('--geometry', type=str, help='Option for choosing sites.', choices=['point', 'grid', 'state'])
-parser.add_argument('--save_resource', help='Save resource data', action='store_true')
+parser.add_argument('--api_key', type=str, help='NREL API Key. Sign up @ https://developer.nrel.gov/signup/')
+parser.add_argument('--email', type=str, help='Email address.')
+parser.add_argument('--geometry', type=str, help='Option for choosing sites.', choices=['grid', 'state'])
 parser.add_argument('--verbose', action='store_true')
-parser.add_argument('--lat', type=float, help='Required if geometry=point')
-parser.add_argument('--lon', type=float, help='Required if geometry=point')
-parser.add_argument('--min_lat', type=float)
-parser.add_argument('--max_lat', type=float)
-parser.add_argument('--min_lon', type=float)
-parser.add_argument('--max_lon', type=float)
-parser.add_argument('--states', nargs='+', type=str, help='Required if geometry=state, e.g. \'PA OH NY\'')
-parser.add_argument('--deg_resolution', type=float, default=.04, help='Approximate km resolution of grid. Used for '
-                                                                      'geometry=state or geometry=grid, default .04')
+parser.add_argument('--min_lat', type=float, help='Required if geometry=grid')
+parser.add_argument('--max_lat', type=float, help='Required if geometry=grid')
+parser.add_argument('--min_lon', type=float, help='Required if geometry=grid')
+parser.add_argument('--max_lon', type=float, help='Required if geometry=grid')
+parser.add_argument('--states', nargs='+', type=str,
+                    help="Required if geometry=state, e.g. 'PA OH NY'.. Input == 'CONTINENTAL' for entire US.")
+parser.add_argument('--deg_resolution', type=float, default=.04, help='Approximate resolution of coordinate grid. '
+                                                                      'Used for geometry=state or geometry=grid,'
+                                                                      'default .04')
+# Following arguments needed when running in PyCharm
+parser.add_argument("--mode", default='client')
+parser.add_argument("--port", default=50071)  # change port default as needed
 
 args = parser.parse_args()
 
-user_name = 'mr_smith'
-user_path = f'/Users/{user_name}/Desktop/'
-local_path = os.path.dirname(__file__)
 
+def getWindData(year, lat, lon):
+    """ by year and coordinate --> retrieve wind resource data and wind speed class from NREL's WIND Toolkit """
 
-def getWindCAPEX(year, lat, lon):
-    """ calculate land-based wind capital expenditure (CAPEX) based on geographic location (average wind speed) """
+    windCSV = os.path.join(local_path, f'windData/{lat}_{lon}_wtk.csv')
 
-    windSRW = os.path.join(local_path, 'resourceData/{lat}_{lon}_wtk.srw'.format(lat=lat, lon=lon))
-
-    if not os.path.exists(windSRW):
+    if not os.path.exists(windCSV):
         wtk_url = 'https://developer.nrel.gov/api/wind-toolkit/v2/wind/wtk-srw-download'
 
         params = {'api_key': args.api_key,
@@ -49,54 +49,72 @@ def getWindCAPEX(year, lat, lon):
                   'utc': 'true'
                   }
 
-        params_str = '&'.join(['{key}={value}'.format(key=key, value=params[key]) for key in params])
-        download_url = '{wtk_url}?{params_str}'.format(wtk_url=wtk_url, params_str=params_str)
+        params_str = '&'.join([f'{key}={params[key]}' for key in params])
+        download_url = f'{wtk_url}?{params_str}'
+        print(download_url)
 
+        # Save resource CSV files
         windResource = pd.read_csv(download_url)
-
-        # Process for SAM
-        windResourceDescription = windResource.columns.values.tolist()
-
-        # Save srw
-        windResource.to_csv(windSRW, index=False)
+        windResource.to_csv(windCSV, index=False)
 
     # Find wind speed
-    windSpeed100 = np.median(pd.read_csv(windSRW, skiprows=[0, 1, 3, 4], usecols=['Speed']).values)
+    windSpeed100 = np.median(pd.read_csv(windCSV, skiprows=[0, 1, 3, 4], usecols=['Speed']).values)
 
     # Adapted from NREL ATB 2020
     if windSpeed100 >= 9.01:
         windClass = 1
+        CAPEX = 1642.02
     elif windSpeed100 >= 8.77:
         windClass = 2
+        CAPEX = 1523.73
     elif windSpeed100 >= 8.57:
         windClass = 3
+        CAPEX = 1492.62
     elif windSpeed100 >= 8.35:
         windClass = 4
+        CAPEX = 1483.59
     elif windSpeed100 >= 8.07:
         windClass = 5
+        CAPEX = 1522.56
     elif windSpeed100 >= 7.62:
         windClass = 6
+        CAPEX = 1665.03
     elif windSpeed100 >= 7.1:
         windClass = 7
+        CAPEX = 1830.93
     elif windSpeed100 >= 6.53:
         windClass = 8
+        CAPEX = 2168.57
     elif windSpeed100 >= 5.9:
         windClass = 9
+        CAPEX = 2548.91
     elif windSpeed100 >= 1.72:
         windClass = 10
+        CAPEX = 2690.06
     else:
         windClass = 'NA'
+        CAPEX = 'NA'
 
-    # TODO: add CAPEX values to column based on wind-class
-
-    # Local file output
-    windClassOUT = os.path.join(user_path, 'windClassOUT.csv')
-    windSRW.to_csv(windClassOUT, index=False)
-
-    return windSRW, windClass
+    return lat, lon, windSpeed100, windClass, CAPEX
 
 
-def getStateCoords():
+def getWindCosts():
+    """ calculate land-based wind capital expenditure (CAPEX) based on geographic location (average wind speed) """
+
+
+def getCoords():
+    if args.geometry == 'grid':
+        coordinates = []
+
+        lat = args.min_lat
+        while lat <= args.max_lat:
+            lon = args.min_lon
+            while lon <= args.max_lon:
+                coordinates.append((lat, lon))
+                lon += args.deg_resolution
+            lat += args.deg_resolution
+        return coordinates
+
     states = args.states
     if 'CONTINENTAL' in args.states:
         states = ['AL', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'ID', 'IL',
@@ -104,20 +122,22 @@ def getStateCoords():
                   'MT', 'NE', 'NV', 'NH', 'NH', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR',
                   'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI',
                   'WY']
+
     # get outer bounds
     usShp = gpd.read_file(os.path.join(local_path, 'states/s_11au16.shp'))
     statesShp = usShp[usShp['STATE'].isin(states)]
-    bounds = statesShp.total_bounds
 
     coordinates = []
 
     bounds = statesShp.total_bounds
+
     min_lon = round(bounds[0], 2)
     min_lat = round(bounds[1], 2)
     max_lon = round(bounds[2], 2)
     max_lat = round(bounds[3], 2)
+
     lat = min_lat
-    print(lat)
+
     while lat <= max_lat:
         lon = min_lon
         while lon <= max_lon:
@@ -130,7 +150,18 @@ def getStateCoords():
 
 
 def main():
-    print(getStateCoords())
+
+    print(local_path)
+    print(getCoords())
+
+    year = args.year
+    coords = getCoords()
+    print(f'{len(coords)} coordinates found...')
+
+    for i in range(len(coords)):
+        lat = coords[i][0]
+        lon = coords[i][1]
+        print(getWindData(year, lat, lon))
 
 
 if __name__ == '__main__':
