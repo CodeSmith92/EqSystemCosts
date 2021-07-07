8c99deca88f07d9e8606c7dd39cd914c3bdcdc6b
DC Smith <45472555+CodeSmith92@users.noreply.github.com>
2021-07-07 02:29:56 -0400

    Update coal.py

--- coal.py
+++ coal.py
@@ -3,13 +3,15 @@ import os
 
 
 local_path = os.path.dirname(os.path.abspath(__file__))
-coal_output = os.path.join(local_path, 'coal_data_output/' )
+coal_output = os.path.join(local_path, 'coal_data_output/')
+
 
 def getPlantList():
     """ subsets and cleans EIA-923 page 1 data for use in decarb + equity optimization model """
 
     # Import Comprehensive Power Plant List From EIA-923 Page 1
-    cpl = pd.read_csv("EIA923GenFuel2020.csv")
+    file_path = os.path.join(local_path, 'coal_plant_data/EIA923GenFuel2020.csv')
+    cpl = pd.read_csv(file_path)
 
     # Subset coal plants by AER code
     cpl = cpl[
@@ -31,7 +33,7 @@ def getPlantList():
     cpl = cpl[(cpl.EIA_Sector != 3) & (cpl.EIA_Sector != 7)]
     cpl = cpl[cpl['Combined_Heat'] == 'N']
 
-    # Sum individual generator consumption, and generation for plant totals
+    # Sum individual generator consumption, and and individual generator generation for plant totals
     cpl.loc[:, 'NetGen_MWh'] = cpl.groupby(['ORIS_ID'])['Gen_MWh'].transform('sum')
     cpl.loc[:, 'NetFuelCon_MMBTU'] = cpl.groupby(['ORIS_ID'])['FuelCon_MMBTU'].transform('sum')
 
@@ -55,7 +57,8 @@ def getRegCoalCosts():
     cpl = getPlantList()
 
     # Load fuel cost data for coal plants from EIA-923 page 5
-    fcl = pd.read_csv("EIA923FuelCosts2020.csv")
+    file_path = os.path.join(local_path, 'coal_plant_data/EIA923FuelCosts2020.csv')
+    fcl = pd.read_csv(file_path)
 
     fcl = fcl[
         ['YEAR', 'MONTH', 'Plant Id', 'Plant Name', 'FUEL_GROUP', 'Regulated', 'Average Heat\nContent', 'FUEL_COST']]
@@ -66,30 +69,30 @@ def getRegCoalCosts():
     fcl = fcl[fcl['FUEL_GROUP'] == 'Coal']
     print("Average heat content for all coal plants (2020): ", fcl['Avg_Heat_Content'].mean())
 
-    fcl_reg = fcl[fcl['Regulated'] == 'REG']
+    fcl = fcl[fcl['Regulated'] == 'REG']
 
     # Clean
-    del fcl_reg['Avg_Heat_Content']
-    fcl_reg = fcl_reg.astype({"FUEL_COST": float})
+    del fcl['Avg_Heat_Content']
+    fcl = fcl.astype({'FUEL_COST': float})
 
-    fcl_reg = fcl_reg.groupby('ORIS_ID')["FUEL_COST"].mean().rename("Avg_Fuel_Cost").reset_index()
+    fcl_reg = fcl.groupby('ORIS_ID')["FUEL_COST"].mean().rename("Avg_Fuel_Cost").reset_index()
     fcl_reg['Avg_Fuel_Cost'] = fcl_reg['Avg_Fuel_Cost'] / 100  # for units of $/MMBTU
 
     # Merge dataframes based on ORIS ID
     coalCostsReg = pd.merge(cpl, fcl_reg, on=['ORIS_ID'])
 
     # Calculate annualized marginal cost of fuel
-    coalCostsReg['Heat_Rate'] = coalCostsReg['NetFuelCon_MMBTU'] / coalCostsReg['NetGen_MWh']
-    coalCostsReg['Marginal_Fuel_Cost'] = (coalCostsReg['Heat_Rate'] * coalCostsReg['Avg_Fuel_Cost'])
+    coalCostsReg.loc[:, 'Heat_Rate'] = coalCostsReg['NetFuelCon_MMBTU'] / coalCostsReg['NetGen_MWh']
+    coalCostsReg.loc[:, 'Marginal_Fuel_Cost'] = (coalCostsReg['Heat_Rate'] * coalCostsReg['Avg_Fuel_Cost'])
 
     # Parameter values taken from Lazard and NREL ATB 2020
-    coalCostsReg['VOM'] = 4.5  # $/MWh
-    coalCostsReg['Coal_VOPEX'] = coalCostsReg['Marginal_Fuel_Cost'] + coalCostsReg['VOM']  # $/MWh
-    coalCostsReg['Coal_FOPEX'] = 40  # $/kW-yr
+    coalCostsReg.loc[:, 'VOM'] = 4.5  # $/MWh
+    coalCostsReg.loc[:, 'Coal_VOPEX'] = coalCostsReg['Marginal_Fuel_Cost'] + coalCostsReg['VOM']  # $/MWh
+    coalCostsReg.loc[:, 'Coal_FOPEX'] = 40  # $/kW-yr
 
     # Local file output
-    ccr_output = os.path.join(coal_output, 'CoalCostsReg.csv')
-    coalCostsReg.to_csv(ccr_output, index=False)
+    reg_output = os.path.join(coal_output, 'CoalCostsReg.csv')
+    coalCostsReg.to_csv(reg_output, index=False)
 
     return coalCostsReg
 
@@ -102,39 +105,40 @@ def getUnrCoalCosts():
     cpl = getPlantList()
 
     # Load fuel cost data for coal plants from EIA-923 page 5
-    fcl = pd.read_csv("EIA923FuelCosts2020.csv")
+    file_path = os.path.join(local_path, 'coal_plant_data/EIA923FuelCosts2020.csv')
+    fcl = pd.read_csv(file_path)
 
     fcl = fcl[
-        ['YEAR', 'MONTH', 'Plant Id', 'Plant Name', 'FUEL_GROUP', 'Regulated', 'Average Heat\nContent', 'FUEL_COST']]
+        ['YEAR', 'MONTH', 'Plant Id', 'Plant Name', 'FUEL_GROUP', 'Regulated', 'Average Heat\nContent']]
     fcl.rename(columns={'Plant Id': 'ORIS_ID', 'Average Heat\nContent': 'Avg_Heat_Content'}, inplace=True)
 
     # Filter out regulated coal plants
-    fcl_unr = fcl[fcl['Regulated'] == 'UNR']
+    fcl = fcl[fcl['FUEL_GROUP'] == 'Coal']
+    fcl = fcl[fcl['Regulated'] == 'UNR']
 
     # Check average heat content of unregulated coal plants specifically
-    print("Average heat content of UNR coal plants: ", fcl_unr['Avg_Heat_Content'].mean())
+    print("Average heat content of UNR coal plants: ", fcl['Avg_Heat_Content'].mean())
 
-    # Clean
-    del fcl_unr['FUEL_COST']
+    fcl.loc[:, 'Fuel_Cost'] = 1.925  # Calculated outside of code
 
-    fcl_unr['Fuel_Cost'] = 1.925  # Calculated outside of code
-    fcl_unr.groupby('ORIS_ID')["FuelCost"].mean().rename("Avg_Fuel_Cost").reset_index()
+    fcl = fcl.astype({'Fuel_Cost': float})
+    fcl_unr = fcl.groupby('ORIS_ID')['Fuel_Cost'].mean().rename('Avg_Fuel_Cost').reset_index()
 
     # Merge dataframes based on ORIS ID
     coalCostsUnr = pd.merge(cpl, fcl_unr, on=['ORIS_ID'])
 
     # Calculate annualized marginal cost of fuel
-    coalCostsUnr['Heat_Rate'] = coalCostsUnr['NetFuelCon_MMBTU'] / coalCostsUnr['NetGen_MWh']
-    coalCostsUnr['Marginal_Fuel_Cost'] = (coalCostsUnr['Heat_Rate'] * coalCostsUnr['Avg_Fuel_Cost'])
+    coalCostsUnr.loc[:, 'Heat_Rate'] = coalCostsUnr['NetFuelCon_MMBTU'] / coalCostsUnr['NetGen_MWh']
+    coalCostsUnr.loc[:, 'Marginal_Fuel_Cost'] = (coalCostsUnr['Heat_Rate'] * coalCostsUnr['Avg_Fuel_Cost'])
 
     # Parameter values taken from Lazard and NREL ATB 2020
-    coalCostsUnr['VOM'] = 4.5  # $/MWh
-    coalCostsUnr['Coal_VOPEX'] = coalCostsUnr['Marginal_Fuel_Cost'] + coalCostsUnr['VOM']  # $/MWh
-    coalCostsUnr['Coal_FOPEX'] = 40  # $/kW-yr
+    coalCostsUnr.loc[:, 'VOM'] = 4.5  # $/MWh
+    coalCostsUnr.loc[:, 'Coal_VOPEX'] = coalCostsUnr['Marginal_Fuel_Cost'] + coalCostsUnr['VOM']  # $/MWh
+    coalCostsUnr.loc[:, 'Coal_FOPEX'] = 40  # $/kW-yr
 
     # Local file output
-    ccu_output = os.path.join(coal_output, 'CoalCostsUnr.csv')
-    coalCostsUnr.to_csv(ccu_output, index=False)
+    unr_output = os.path.join(coal_output, 'CoalCostsUnr.csv')
+    coalCostsUnr.to_csv(unr_output, index=False)
 
     return coalCostsUnr
 
@@ -148,7 +152,23 @@ def mergeCosts():
     costsTotal = pd.concat([costsReg, costsUnr], ignore_index=True)
 
     # Local file output
-    cct_output = os.path.join(coal_output, 'coal_costs_total.csv')
-    costsTotal.to_csv(cct_output, index=False)
+    cost_output = os.path.join(coal_output, 'coal_costs_total.csv')
+    costsTotal.to_csv(cost_output, index=False)
 
     return costsTotal
+
+
+def main():
+
+    print(local_path)
+    cwd = os.getcwd()  # Get the current working directory (cwd)
+    files = os.listdir(cwd)  # Get all the files in that directory
+    print("Files in %r: %s" % (cwd, files))
+
+    print(mergeCosts())
+    print('Finished!')
+
+
+if __name__ == '__main__':
+    main()
+
